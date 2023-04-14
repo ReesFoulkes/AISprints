1a740e7a5d8e2727ac02353795d46946f526fc8e
ReesFoulkes <reesfoulkes@outlook.com>
2023-04-14 12:13:58 +0100

    reverting

--- pages/api/google.ts
+++ pages/api/google.ts
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+
 import { OPENAI_API_HOST } from '@/utils/app/const';
 import { cleanSourceText } from '@/utils/server/google';
+
 import { Message } from '@/types/chat';
 import { GoogleBody, GoogleSource } from '@/types/google';
+
 import { Readability } from '@mozilla/readability';
 import endent from 'endent';
 import jsdom, { JSDOM } from 'jsdom';
-import pLimit from 'p-limit';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   try {
@@ -21,7 +23,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
         googleAPIKey ? googleAPIKey : process.env.GOOGLE_API_KEY
       }&cx=${
         googleCSEId ? googleCSEId : process.env.GOOGLE_CSE_ID
-      }&q=${query}&num=3`, // Reducing num to 3
+      }&q=${query}&num=5`,
     );
 
     const googleData = await googleRes.json();
@@ -35,41 +37,49 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
       text: '',
     }));
 
-    const limit = pLimit(2); // limit the number of concurrent requests to 2
-
     const sourcesWithText: any = await Promise.all(
-      sources.map((source) =>
-        limit(async () => {
-          try {
-            const timeoutPromise = new Promise((_, reject) =>
-              setTimeout(() => reject(new Error('Request timed out')), 5000), // Reducing timeout to 5s
-            );
+      sources.map(async (source) => {
+        try {
+          const timeoutPromise = new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('Request timed out')), 5000),
+          );
 
-            const res = (await Promise.race([
-              fetch(source.link),
-              timeoutPromise,
-            ])) as any;
-            const html = await res.text();
+          const res = (await Promise.race([
+            fetch(source.link),
+            timeoutPromise,
+          ])) as any;
 
-            const dom = new JSDOM(html);
-            const doc = dom.window.document;
-            const parsed = new Readability(doc).parse();
+          // if (res) {
+          const html = await res.text();
 
-            if (parsed) {
-              const sourceText = cleanSourceText(parsed.textContent);
-
-              return {
-                ...source,
-                text: sourceText.slice(0, 2000),
-              } as GoogleSource;
+          const virtualConsole = new jsdom.VirtualConsole();
+          virtualConsole.on('error', (error) => {
+            if (!error.message.includes('Could not parse CSS stylesheet')) {
+              console.error(error);
             }
-            return null;
-          } catch (error) {
-            console.error(error);
-            return null;
+          });
+
+          const dom = new JSDOM(html, { virtualConsole });
+          const doc = dom.window.document;
+          const parsed = new Readability(doc).parse();
+
+          if (parsed) {
+            let sourceText = cleanSourceText(parsed.textContent);
+
+            return {
+              ...source,
+              // TODO: switch to tokens
+              text: sourceText.slice(0, 2000),
+            } as GoogleSource;
           }
-        }),
-      ),
+          // }
+
+          return null;
+        } catch (error) {
+          console.error(error);
+          return null;
+        }
+      }),
     );
 
     const filteredSources: GoogleSource[] = sourcesWithText.filter(Boolean);
